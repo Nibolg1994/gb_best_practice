0a2e6e90c5ba8fc2f5181cd8cfc94f5e31fe363b
Nibolg1994 <globin.anvi@gmail.com>
2022-06-01 22:51:00 +0300

    added tests

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-//Исходники задания для первого занятия у других групп https://github.com/t0pep0/GB_best_go
 import (
 	"context"
 	"fmt"
@@ -9,31 +8,11 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	. "practic/filesystem"
 	"runtime"
 	"syscall"
 	"time"
 )
-
-type TargetFile struct {
-	Path string
-	Name string
-}
-
-type FileList map[string]TargetFile
-
-type FileInfo interface {
-	os.FileInfo
-	Path() string
-}
-
-type fileInfo struct {
-	os.FileInfo
-	path string
-}
-
-func (fi fileInfo) Path() string {
-	return fi.path
-}
 
 func GetLogger() *zap.Logger {
 	config := zap.NewProductionConfig()
@@ -42,68 +21,6 @@ func GetLogger() *zap.Logger {
 	logger, _ := config.Build()
 	logger = logger.With(zap.String("goos", runtime.GOOS))
 	return logger
-}
-
-func ListDirectory(logger *zap.Logger, cancelCtx context.Context, userChan chan struct{}, dir string, depth int) ([]FileInfo, error) {
-
-	if logger != nil {
-		logger.With(
-			zap.String("time", time.Now().String()),
-			zap.String("dir", dir),
-		).Debug("ListDirectory, call")
-	}
-
-	select {
-	case <-cancelCtx.Done():
-		if logger != nil {
-			logger.With(
-				zap.String("time", time.Now().String()),
-			).Debug("Context closed")
-		}
-		return nil, nil
-	case <-userChan:
-		if logger != nil {
-			logger.With(
-				zap.String("time", time.Now().String()),
-				zap.String("dir", dir),
-				zap.String("depth", dir),
-			).Debug("processing User signal")
-		}
-	}
-
-	time.Sleep(time.Second * 2)
-	var result []FileInfo
-	res, err := os.ReadDir(dir)
-	if logger != nil {
-		logger.With(
-			zap.String("time", time.Now().String()),
-		).Debug("Found file ...")
-	}
-	if err != nil {
-		return nil, err
-	}
-	for _, entry := range res {
-		fmt.Println(entry.Name())
-		path := filepath.Join(dir, entry.Name())
-		if logger != nil {
-			logger.With(
-				zap.String("time", time.Now().String()),
-				zap.String("path", path),
-			).Debug("Found file ...")
-		}
-		if entry.IsDir() {
-			depth++
-			child, err := ListDirectory(logger, cancelCtx, userChan, path, depth) //Дополнительно: вынести в горутину
-			if err != nil {
-				return nil, err
-			}
-			result = append(result, child...)
-		} else {
-			info, _ := entry.Info()
-			result = append(result, fileInfo{info, path})
-		}
-	}
-	return result, nil
 }
 
 func FindFiles(logger *zap.Logger, cancelCtx context.Context, userChan chan struct{}, ext string) (FileList, error) {
--- tests/list_directory_test.go
+++ tests/list_directory_test.go
@@ -1,27 +1,38 @@
-package main
+package tests
 
 import (
 	"fmt"
-	testify "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify"
 	"io/fs"
 	"os"
 	"strings"
 	"testing"
 	"time"
+	."practic/filesystem"
 )
-
 
 type FileNode struct {
 	size  int64
 	name  string
 	isDir bool
-	files []*FileNode
+}
+
+type FileEntryNode struct {
+	currentNode FileNode
+	nodes []*FileNode
 }
 
 func (f *FileNode) Name() string {
 	return f.name
 }
 
+func (f *FileNode) Type() fs.FileMode {
+	return 0
+}
+
+func (f *FileNode) IsDir() bool {
+	return f.isDir
+}
 func (f *FileNode) Size() int64 {
 	return f.size
 }
@@ -34,32 +45,44 @@ func (f *FileNode) ModTime() time.Time {
 	return time.Now()
 }
 
-func (f *FileNode) IsDir() bool {
-	return f.isDir
-}
-
 func (f *FileNode) Sys() interface{} {
 	return nil
 }
 
-func (f *FileNode) getFile(name string) (result bool, node *FileNode) {
+func (f *FileEntryNode) Info() (fs.FileInfo, error) {
+	return &f.currentNode, nil
+}
+
+func (f *FileEntryNode) Name() string {
+	return f.Name()
+}
+
+func (f *FileEntryNode) IsDir() bool {
+	return f.IsDir()
+}
+
+func (f *FileEntryNode) Type()  fs.FileMode {
+	return f.Type()
+}
+
+func (f *FileEntryNode) getFile(name string) (result bool, node *FileEntryNode) {
 	result = false
 	node = nil
-	if f.name == name {
+	if f.Name() == name {
 		return true, f
 	}
 
-	for _, file := range f.files {
-		if file.name == name {
+	for _, file := range f.nodes {
+		if file.Name() == name {
 			result = true
-			node = file
+			node = &FileEntryNode{currentNode: *file, nodes: nil}
 			break
 		}
 	}
 	return result, node
 }
 
-func (s *FileNode) ReadDir(dirname string) ([]fs.FileInfo, error) {
+func (s *FileEntryNode) ReadDir(dirname string) ([]fs.DirEntry, error) {
 	items := strings.Split(dirname, string(os.PathSeparator))
 	file := s
 	ok := false
@@ -76,7 +99,7 @@ func (s *FileNode) ReadDir(dirname string) ([]fs.FileInfo, error) {
 		}
 	}
 
-	list := []fs.FileInfo{}
+	list := []fs.DirEntry{}
 
 	for _, fileItem := range file.files {
 		list = append(list, fileItem)
@@ -88,7 +111,7 @@ func TestFileSystem(t *testing.T) {
 	root := FileNode{size: 0, name: "root", isDir: true, files: []*FileNode{}}
 	varDir := FileNode{size: 0, name: "var", isDir: true, files: []*FileNode{}}
 	systemDir := FileNode{size: 0, name: "system", isDir: true, files: []*FileNode{}}
-	root.files = append(
+	root.n = append(
 		root.files,
 		&varDir,
 		&systemDir,
@@ -123,7 +146,6 @@ func TestFileSystem(t *testing.T) {
 	if err == nil {
 		t.Fatalf("can not read file as dir")
 	}
-
 }
 
 func TestListDirectory(t *testing.T) {
@@ -149,9 +171,10 @@ func TestListDirectory(t *testing.T) {
 		&FileNode{size: 0, name: "b.txt", isDir: false, files: []*FileNode{}},
 	)
 
+
 	testify.Equal(
 		t,
 		[]string{"/root/system/a.txt", "/root/system/b.txt", "/root/var/a.txt", "/root/var/b.txt"},
-		ListDirectory(),
+		ListDirectory(nil, nil, nil, nil, nil),
 	)
 }
